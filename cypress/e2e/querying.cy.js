8ec4eaa9dcfdd22318484778ea1fd23a187d4e2c
cir94 <claire.isabella.ramos@gmail.com>
2024-04-25 16:33:38 -0400

    feat: created test in querying for using cy.contains()

--- cypress/e2e/querying.cy.js
+++ cypress/e2e/querying.cy.js
@@ -1,9 +1,13 @@
 describe('Kitchen Sink - Querying', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/querying')
-  })
-  it('should query the button and div via cy.get() command', function() {
+    cy.visit('https://example.cypress.io/commands/querying');
+  });
+  it('should query the button and div via cy.get() command', function () {
     cy.get('#query-btn').should('contain', 'Button');
-    cy.get('[data-test-id="test-example"]').should('have.class', 'example')
-  })
-})
+    cy.get('[data-test-id="test-example"]').should('have.class', 'example');
+  });
+
+  it('should find elements by their contents via cy.contains()', function () {
+    cy.get('.query-list').contains('apples').should('have.class', 'first');
+  });
+});
