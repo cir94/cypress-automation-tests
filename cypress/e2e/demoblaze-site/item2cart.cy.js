905d620b13fdd7274588936e31c975d3548ea594
cir94 <claire.isabella.ramos@gmail.com>
2024-05-03 10:27:51 -0400

    feat: added assertion to ensure correct item is in cart for item2cart.cy.js

--- cypress/e2e/demoblaze-site/item2cart.cy.js
+++ cypress/e2e/demoblaze-site/item2cart.cy.js
@@ -1,9 +1,6 @@
 describe('Demoblaze - Adding item to cart and purchasing', function () {
-  beforeEach(() => {
-    cy.visit(`https://www.demoblaze.com/index.html#`);
-  });
-
   it('should click on the Nexus 6 item on the homepage, add it to the cart, and confirm it is there before purchasing', function () {
+    cy.visit(`https://www.demoblaze.com/index.html#`);
     cy.contains('Nexus 6').click();
     cy.get('.name').should('have.text', 'Nexus 6');
     cy.contains('Add to cart').should('be.visible').click();
@@ -11,6 +8,6 @@ describe('Demoblaze - Adding item to cart and purchasing', function () {
       expect(text).to.eq('Product added');
     });
     cy.get('#cartur').should('have.text', 'Cart').click();
-    cy.get('#tbodyid').children().contains('Nexus 6');
+    cy.get('#tbodyid').children().should('contain', 'Nexus 6');
   });
 });
