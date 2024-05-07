f8190a1892eb23306895f08d841188150d88b35c
cir94 <claire.isabella.ramos@gmail.com>
2024-05-07 16:18:31 -0400

    feat: added tests to check Laptops and Monitors category in homepage for homepage-category.cy.js spec file

--- cypress/e2e/demoblaze-site/homepage-category.cy.js
+++ cypress/e2e/demoblaze-site/homepage-category.cy.js
@@ -12,4 +12,28 @@ describe('Demoblaze - Changing Categories on Homepage', function () {
       .should('contain', 'Samsung galaxy s6');
     cy.get('#tbodyid').children().last().should('contain', 'HTC One M9');
   });
+
+  it('should click on the laptops category and ensure they are visible on page', function () {
+    cy.get(`a[onclick*="byCat('notebook')"`)
+      .should('have.text', 'Laptops')
+      .click();
+    cy.get('#tbodyid')
+      .children()
+      .should('have.length', 6)
+      .first()
+      .should('contain', 'Sony vaio i5');
+    cy.get('#tbodyid').children().last().should('contain', 'MacBook Pro');
+  });
+
+  it('should click on the monitors category and ensure they are visible on page', function () {
+    cy.get(`a[onclick*="byCat('monitor')"]`)
+      .should('have.text', 'Monitors')
+      .click();
+    cy.get('#tbodyid')
+      .children()
+      .should('have.length', 2)
+      .first()
+      .should('contain', 'Apple monitor 24');
+    cy.get('#tbodyid').children().last().should('contain', 'ASUS Full HD');
+  });
 });
