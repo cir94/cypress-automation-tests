3493785b8a436f37010bda0d0aa6b940e01d6a2a
cir94 <claire.isabella.ramos@gmail.com>
2024-05-08 18:46:52 -0400

    refactor: Refactored the code in user-pass-randomizer.js to separate functions that create username and password for exporting purposes. Changed the functions to export const to work in other spec files if necessary

--- cypress/e2e/demoblaze-site/user-pass-randomizer.js
+++ cypress/e2e/demoblaze-site/user-pass-randomizer.js
@@ -1,25 +1,26 @@
 'use strict';
 
-let username = '';
-let password = '';
-
-let userpassRandomizer = function () {
+export const userRandomizer = function () {
   const words = ['Test', 'User', 'Cy', 'Cypress', 'Dummy'];
 
-  // Adding a randomly selected word from the words array with Math.floor to username and password variables respectively
-  username = words[`${Math.floor(Math.random() * 5)}`];
-  password = words[`${Math.floor(Math.random() * 5)}`];
+  // Adding a randomly selected word from the words array with Math.floor to username and password variables respectively in each function
+  let username = words[`${Math.floor(Math.random() * 5)}`];
 
-  // The following for statements add random numbers to the username and password variables respectively
+  // The following for statements add random numbers to the username and password variables respectively for each function
   for (let x = 0; x < 4; x++) {
     let num = Math.floor(Math.random() * 9);
     username += num;
   }
+  return username;
+};
+
+export const passRandomizer = function () {
+  const words = ['Test', 'User', 'Cy', 'Cypress', 'Dummy'];
+  let password = words[`${Math.floor(Math.random() * 5)}`];
 
   for (let x = 0; x < 4; x++) {
     let num = Math.floor(Math.random() * 51);
     password += num;
   }
+  return password;
 };
-
-userpassRandomizer();
