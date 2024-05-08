509e8aef60e710d1b4a291f282cd9092d6d10bbe
cir94 <claire.isabella.ramos@gmail.com>
2024-05-08 18:00:38 -0400

    refactor: removed unnecessary console logs from user-pass-randomizer.js and added comments to explain code

--- cypress/e2e/demoblaze-site/user-pass-randomizer.js
+++ cypress/e2e/demoblaze-site/user-pass-randomizer.js
@@ -7,6 +7,9 @@ let userpassRandomizer = function () {
   const words = ['Test', 'User', 'Cy', 'Cypress', 'Dummy'];
   username = words[`${Math.floor(Math.random() * 5)}`];
   password = words[`${Math.floor(Math.random() * 5)}`];
+
+  // The following for statements add random numbers to the username and password variables respectively
+
   for (let x = 0; x < 4; x++) {
     let num = Math.floor(Math.random() * 9);
     username += num;
@@ -16,11 +19,6 @@ let userpassRandomizer = function () {
     let num = Math.floor(Math.random() * 51);
     password += num;
   }
-
-  return username, password;
 };
 
 userpassRandomizer();
-
-console.log(username);
-console.log(password);
