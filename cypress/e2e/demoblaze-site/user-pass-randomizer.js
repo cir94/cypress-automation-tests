4a4789894b5665ce8044c982619080291e7a6f97
cir94 <claire.isabella.ramos@gmail.com>
2024-05-22 15:13:02 -0400

    refactor: user-pass-randomizer.js - added another word to words array and increased number of digits added to end of username for the randomizer to use when creating a username

--- cypress/e2e/demoblaze-site/user-pass-randomizer.js
+++ cypress/e2e/demoblaze-site/user-pass-randomizer.js
@@ -1,13 +1,13 @@
 'use strict';
 
 export const userRandomizer = function () {
-  const words = ['Test', 'User', 'Cy', 'Cypress', 'Dummy'];
+  const words = ['Test', 'Auto', 'User', 'Cy', 'Cypress', 'Dummy'];
 
   // Adding a randomly selected word from the words array with Math.floor to username and password variables respectively in each function
-  let username = words[`${Math.floor(Math.random() * 5)}`];
+  let username = words[`${Math.floor(Math.random() * 6)}`];
 
   // The following for statements add random numbers to the username and password variables respectively for each function
-  for (let x = 0; x < 4; x++) {
+  for (let x = 0; x < 6; x++) {
     let num = Math.floor(Math.random() * 9);
     username += num;
   }
