92a67596212daff25d74b5fd78a7a7a84fef51cb
cir94 <claire.isabella.ramos@gmail.com>
2024-06-14 14:07:37 -0400

    feat: finished prelim test for learning how API calls work in Cypress

--- cypress/e2e/api-site-test/api-test.cy.js
+++ cypress/e2e/api-site-test/api-test.cy.js
@@ -1,3 +1,11 @@
 context('Test API', () => {
-  it('should get a bed of data from given value', () => {});
+  it('should get a bed of data from given value', () => {
+    cy.request('GET', 'https://reqres.in/api/users?page=2').then((response) => {
+      expect(response.status).to.eq(200);
+      // console.log(response.body);
+      expect(response.body.data).length.to.be.greaterThan(1);
+      expect(response.body.data[0].email).to.equal('michael.lawson@reqres.in');
+      console.log(response.body.data);
+    });
+  });
 });
